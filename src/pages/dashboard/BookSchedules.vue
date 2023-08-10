19ca7389ecd1160de172271fb6f5a13cd89fac83
HasegawaTaizou <caiocoghi@gmail.com>
2023-08-10 20:06:22 -0300

    alo

--- src/pages/dashboard/BookSchedules.vue
+++ src/pages/dashboard/BookSchedules.vue
@@ -12,6 +12,36 @@
       </div>
     </div>
     <div class="book-schedules__content">
+      <div class="book-scheduling-container">
+        <div class="book-scheduling">
+          <div class="scheduling-date">
+            <span class="date__title">Escolha a data e hora:</span>
+            <input
+              type="datetime-local"
+              name=""
+              id=""
+              class="date__datetime-local"
+            />
+          </div>
+          <div class="scheduling-site">
+            <span class="site__title">Escolha o local:</span>
+            <select name="" id="" class="site__select">
+              <option value="1">Descrição Local 1</option>
+            </select>
+          </div>
+        </div>
+        <button class="book-scheduling__button">
+          <span class="book-scheduling__text">Reservar agendamento</span>
+          <img
+            src="../../assets/img/plus-icon.png"
+            alt="Book Scheduling Icon"
+            class="book-scheduling__icon"
+          />
+        </button>
+      </div>
+      <div class="schedules-available">
+        <span class="schedules-available__title">Agendamentos disponíveis:</span>
+      </div>
     </div>
   </section>
 </template>
