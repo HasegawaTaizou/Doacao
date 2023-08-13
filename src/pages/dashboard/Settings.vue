62df456df586e69b24a88bf436cc0cf0136e86c8
HasegawaTaizou <caiocoghi@gmail.com>
2023-08-13 11:51:22 -0300

    alo

--- src/pages/dashboard/Settings.vue
+++ src/pages/dashboard/Settings.vue
@@ -13,71 +13,105 @@
     </div>
     <div class="settings__content">
       <div class="hospital-general-data">
-        <img src="../../assets/img/hospital-profile-image.png" alt="Hospital Image" class="hospital-general-data__image">
-        <h2 class="hospital-general-data__title">Hospital Notredame Intermédica</h2>
+        <img
+          src="../../assets/img/hospital-profile-image.png"
+          alt="Hospital Image"
+          class="hospital-general-data__image"
+        />
+        <h2 class="hospital-general-data__title">
+          Hospital Notredame Intermédica
+        </h2>
         <div class="hospital-general-data__cnpj-container">
           <label for="" class="cnpj__label">CNPJ:</label>
-          <input type="text" class="cnpj__input" disabled>
+          <input type="text" class="cnpj__input" disabled />
         </div>
       </div>
       <div class="hospital-contact-data">
         <h2 class="hospital-contact-data__title">Contato:</h2>
         <div class="hospital-contact-data__email-container">
           <label for="" class="email__label">E-mail:</label>
-          <input type="text" class="email__input" disabled>
+          <input type="text" class="email__input" disabled />
         </div>
         <div class="hospital-contact-data__phone-container">
           <label for="" class="phone__label">Telefone:</label>
-          <input type="text" class="phone__input" disabled>
+          <input type="text" class="phone__input" disabled />
         </div>
         <div class="hospital-contact-data__website-container">
           <label for="" class="website__label">Site:</label>
-          <input type="text" class="website__input" disabled>
+          <input type="text" class="website__input" disabled />
         </div>
       </div>
       <div class="hospital-address-data">
         <h2 class="hospital-address-data__title">Endereço:</h2>
         <div class="hospital-address-data__cep-container">
           <label for="" class="cep__label">CEP:</label>
-          <input type="text" class="cep__input" disabled>
+          <input type="text" class="cep__input" disabled />
         </div>
         <div class="hospital-address-data__state-container">
           <label for="" class="state__label">Estado:</label>
-          <input type="text" class="state__input" disabled>
+          <input type="text" class="state__input" disabled />
         </div>
         <div class="hospital-address-data__city-container">
           <label for="" class="city__label">Cidade:</label>
-          <input type="text" class="city__input" disabled>
+          <input type="text" class="city__input" disabled />
         </div>
         <div class="hospital-address-data__neighborhood-container">
           <label for="" class="neighborhood__label">Bairro:</label>
-          <input type="text" class="neighborhood__input" disabled>
+          <input type="text" class="neighborhood__input" disabled />
         </div>
         <div class="hospital-address-data__complement-container">
           <label for="" class="complement__label">Complemento:</label>
-          <input type="text" class="complement__input" disabled>
+          <input type="text" class="complement__input" disabled />
         </div>
       </div>
       <div class="hospital-donation-site-data">
         <h2 class="hospital-donation-site-data__title">Local de doação:</h2>
         <div class="hospital-donation-site-data__site-container">
           <label for="" class="site__label">Local 1:</label>
-          <textarea name="" id="" cols="30" rows="10" class="site__textarea" disabled></textarea>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            class="site__textarea"
+            disabled
+          ></textarea>
         </div>
         <div class="hospital-donation-site-data__other-site-container">
           <label for="" class="other-site__label">Local 2:</label>
-          <textarea name="" id="" cols="30" rows="10" class="other-site__textarea" disabled></textarea>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            class="other-site__textarea"
+            disabled
+          ></textarea>
         </div>
       </div>
       <div class="hospital-actions">
-        <button class="action__redefine-password-container">
+        <router-link
+          :to="'/dashboard/settings/redefine-password'"
+          class="action__redefine-password-container"
+        >
           <span class="redefine-password__text">Redefinir senha</span>
-          <img src="../../assets/img/redefine-password-icon.png" alt="Redefine Password Icon" class="redefine-password__icon">
-        </button>
-        <button class="edit-profile-container">
+          <img
+            src="../../assets/img/redefine-password-icon.png"
+            alt="Redefine Password Icon"
+            class="redefine-password__icon"
+          />
+        </router-link>
+        <router-link
+          :to="'/dashboard/settings/edit-hospital'"
+          class="edit-profile-container"
+        >
           <span class="edit-profile__text">Editar perfil</span>
-          <img src="../../assets/img/edit-profile-icon.png" alt="Edit Profile Icon" class="edit-profile__icon">
-        </button>
+          <img
+            src="../../assets/img/edit-profile-icon.png"
+            alt="Edit Profile Icon"
+            class="edit-profile__icon"
+          />
+        </router-link>
       </div>
     </div>
   </section>
