1e7fdca6d1b1e520f9fd5e85ae718718cae618cf
ODESNILDO FARIAS NOGUEIRA <ODESNILDO.ALUNOS@CFP127ALUNO.LOCAL>
2023-08-19 15:58:20 -0300

    alo

--- src/pages/Pizza.vue
+++ src/pages/Pizza.vue
@@ -1,5 +1,11 @@
-<template>
+<!-- <template>
   <canvas id="myChart"></canvas>
+</template> -->
+
+<template>
+  <div id="teste" style="width: 25  0px; height: 250px">
+    <canvas id="myChart"></canvas>
+  </div>
 </template>
 
 <script>
@@ -10,22 +16,32 @@ export default {
   mounted() {
     const ctx = document.getElementById("myChart");
 
+    const data = {
+      labels: ["Red", "Blue", "Yellow", "Green"],
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [300, 50, 100, 70],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+            "rgb(87, 153, 53)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    };
+
     new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1,
-          },
-        ],
-      },
+      type: "doughnut",
+      data: data,
       options: {
-        scales: {
-          y: {
-            beginAtZero: true,
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
           },
         },
       },
