682e8398c7220099487c6b2fabcd5f0c7f2775b4
HasegawaTaizou <caiocoghi@gmail.com>
2023-10-07 17:23:08 -0300

    alo

--- src/pages/dashboard/Help.vue
+++ src/pages/dashboard/Help.vue
@@ -12,28 +12,34 @@
       </div>
     </div>
     <div class="help__content">
-      <img
-        src="../../assets/img/help-image.png"
-        alt="Help Image"
-        class="help__image"
-      />
-      <ul class="help__questions-container">
-        <li
-          v-for="(question, index) in questions"
-          :key="index"
-          class="question-container"
-        >
-          <h2 class="question__title">{{ question.title }}</h2>
-          <i
-            class="fas"
-            :class="question.open ? 'fa-minus' : 'fa-plus'"
-            @click="toggleQuestion(index)"
-          ></i>
-          <div v-if="question.open" class="question__text-container">
-            <p class="text">{{ question.text }}</p>
+      <div class="help__introduction">
+        <img
+          src="../../assets/img/help-image.png"
+          alt="Help Image"
+          class="help__image"
+        />
+        <p class="help__text">
+          A doação de sangue é um ato de solidariedade e generosidade que pode
+          salvar vidas, e estamos aqui para apoiá-lo em todo o processo.
+        </p>
+      </div>
+      <div class="help__contact">
+        <h2 class="contact__title">Entre em Contato</h2>
+        <p class="contact__text">
+          Se você tiver alguma dúvida, preocupação ou precisar de assistência,
+          nossa equipe de atendimento ao doador está disponível para ajudá-lo.
+        </p>
+        <div class="help__contacts">
+          <div class="contact">
+            <i class="fa-solid fa-phone"></i>
+            <span class="contact__text">(11) 92765-2364</span>
           </div>
-        </li>
-      </ul>
+          <div class="contact">
+            <i class="fa-solid fa-envelope"></i>
+            <span class="contact__text">doevida@gmail.com</span>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -41,29 +47,6 @@
 <script>
 export default {
   name: "Help",
-  data() {
-    return {
-      questions: [
-        {
-          title: "Lorem ipsum dolor sit amet, consec?",
-          text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              odio tellus, pharetra at enim in, pretium hendrerit nisl. Morbi
-              sit amet ultrices risus`,
-        },
-        {
-          title: "Lorem ipsum dolor sit amet, consec?",
-          text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              odio tellus, pharetra at enim in, pretium hendrerit nisl. Morbi
-              sit amet ultrices risus`,
-        },
-      ],
-    };
-  },
-  methods: {
-    toggleQuestion(index) {
-      this.questions[index].open = !this.questions[index].open;
-    },
-  },
 };
 </script>
 
